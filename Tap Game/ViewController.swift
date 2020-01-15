3d03615d0a84ad1082225902d002ef07210eafe8
shyamkpatel <shyamp229@gmail.com>
2020-01-15 15:42:27 -0500

    first commit

--- Tap Game/ViewController.swift
+++ Tap Game/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var player1Label: UILabel!
+    @IBOutlet weak var player2Label: UILabel!
+
+    var playerOneTapCount = 0
+    var playerTwoTapCount = 0
+    var isGameStarted = false
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func player1DidPress(_ sender: Any) {
+        playerOneTapCount += 1
+        updateUI()
+    }
+
+    @IBAction func player2DidPress(_ sender: Any) {
+        playerTwoTapCount += 1
+        updateUI()
+    }
+
+    func updateUI() {
+        if isGameStarted {
+            player1Label.text = "Player 1 Score: \(playerOneTapCount)"
+            player2Label.text = "Player 2 Score: \(playerTwoTapCount)"
+        }
+    }
+
+    @IBAction func startWasPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+
+        isGameStarted = true
+        playerOneTapCount = 0
+        playerTwoTapCount = 0
+        
+        let _ = Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { (timer) in
+            sender.isEnabled = true
+            self.isGameStarted = false
+            
+        }
+        
+    }
 
 }
-
